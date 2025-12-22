b7fe3ad68871d8a63f402804926210ea7d2e03d8
James Holt <jamesphilipdavidholt@gmail.com>
2025-12-22 11:47:02 +0000

    initial

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+	return (
+		<div className="flex min-h-screen items-center justify-center bg-black">
+			<main className="flex flex-col items-center justify-center text-center px-6">
+				<div className="space-y-6">
+					<h1 className="text-5xl md:text-7xl font-bold text-white">
+						Website Under Construction
+					</h1>
+				</div>
+			</main>
+		</div>
+	);
+}
