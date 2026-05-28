adeeb6266889830f5bf644f7178bf1c6ff11d4ba
James Holt <james.holt@hopeforjustice.org>
2026-05-29 00:04:10 +0100

    fix: remove unused interests field from member data in addUpdateSubscriber function

--- src/app/lib/mailchimp/addUpdateSubscriber.js
+++ src/app/lib/mailchimp/addUpdateSubscriber.js
@@ -33,7 +33,6 @@ export default async function addUpdateSubscriber(
 				LNAME: lastname,
 				...additionalMergeFields,
 			},
-			interests,
 		};
 
 		const result = await mailchimp.lists.setListMember(
