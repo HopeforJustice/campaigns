c2a19e98f20dbc3f6e1d7ea99b144c7931b89f63
James Holt <jamesphilipdavidholt@gmail.com>
2026-06-05 15:49:26 +0100

    2026 summer metadata

--- src/app/summer/page.tsx
+++ src/app/summer/page.tsx
@@ -18,10 +18,10 @@ export const metadata: Metadata = {
 		url: "/summer",
 		images: [
 			{
-				url: "/hfj-run.jpg",
+				url: "/2026summerog.png",
 				width: 1200,
 				height: 630,
-				alt: "Sign up to our newsletter",
+				alt: "50 days of hope | Hope for Justice",
 			},
 		],
 	},
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
 		title: "50 days of hope | Hope for Justice",
 		description:
 			"In summer 2026, let’s raise funds to change lives! We say: Impact on human trafficking IS possible. We choose hope. We choose to give our time and energy so others can live in freedom and dignity.",
-		images: ["/hfj-run.jpg"],
+		images: ["/2026summerog.png"],
 	},
 };
 
