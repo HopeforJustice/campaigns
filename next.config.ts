17272003e7bf45cefc9c8e0d980ffddf8ce96e32
James Holt <jamesphilipdavidholt@gmail.com>
2026-06-24 13:23:51 +0100

    fix: add missing headers for X-Forwarded-Host in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
 				{
 					source: "/:path*",
 					destination: "https://testfall.wpenginepowered.com/:path*",
+					headers: [
+						{
+							key: "X-Forwarded-Host",
+							value: "campaigns.hopeforjustice.org",
+						},
+					],
 				},
 			],
 		};
